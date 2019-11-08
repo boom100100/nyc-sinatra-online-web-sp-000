951ad991446189328179596b5f7899b5a7cb5c1a
Bernadette Davis <bsd231@nyu.edu>
2019-11-08 21:29:05 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -4,4 +4,8 @@ class LandmarksController < ApplicationController
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
+
+  post '/landmarks' do
+    @landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+  end
 end
