64fba48434bfcbdca55008ad53b7bf1e9ef3988a
Bernadette Davis <bsd231@nyu.edu>
2019-11-08 21:30:22 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -7,7 +7,9 @@ class LandmarksController < ApplicationController
 
   post '/landmarks' do
     if params[:landmark][:name] != "" && params[:landmark][:year_completed] != ""
-    @landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+      @landmark = Landmark.find_or_create_by(name: params[:landmark][:name], year_completed: params[:landmark][:year_completed])
+    end
+    
     redirect to "/landmarks/#{@landmark.id}"
   end
 end
