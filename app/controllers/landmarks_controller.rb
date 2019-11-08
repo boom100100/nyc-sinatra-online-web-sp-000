7dfbaa91f524ff4e9f8d99aa453d2d359e53d5a3
Bernadette Davis <bsd231@nyu.edu>
2019-11-08 21:34:39 +0000

    Automatically backed up by Learn

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,6 +1,11 @@
 class LandmarksController < ApplicationController
   # add controller methods
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
   get '/landmarks/new' do
     erb :'landmarks/new'
   end
@@ -15,7 +20,7 @@ class LandmarksController < ApplicationController
 
   get '/landmarks/:id' do
     @landmark = Landmark.find(params[:id])
-    
+
     erb :'landmarks/show'
   end
 end
